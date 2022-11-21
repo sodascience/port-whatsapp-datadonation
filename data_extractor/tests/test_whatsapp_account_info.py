dd00d2f89cf3ab344ad8b3382aaf40503906028a
Shiva Nadi <s.nadi@uu.nl>
2022-11-21 14:08:38 +0100

    Add prompt file option

--- data_extractor/tests/test_whatsapp_account_info.py
+++ data_extractor/tests/test_whatsapp_account_info.py
@@ -4,18 +4,24 @@ from pathlib import Path
 import pandas as pd
 from pandas.testing import assert_frame_equal
 
+import pytest
+
 from whatsapp_account_info import process
 
 
-DATA_PATH = Path(__file__).parent / "data"
+DATA_PATH = Path(__file__).parent / "data/account_info"
+FILES_TO_TEST = [p.name for p in DATA_PATH.glob("*.zip")]
+
+
 EXPECTED = [
-    {'number_of_groups': 4,
-     'number_of_contacts': 3
+    {'Aantal groepen': 4,
+     'Aantal contacten': 3
      }
 ]
 
 
-def test_process():
+@pytest.mark.parametrize("filename_to_test", FILES_TO_TEST)
+def test_process(filename_to_test):
     """ Test process function.
         compares the expected dataframe with the output of the process function
         to check if all the columns are matched.
@@ -24,7 +30,13 @@ def test_process():
         AssertionError: When provided expected dataframe could not match the participants dataframe
         """
     df_expected = pd.DataFrame(EXPECTED)
+    file_to_test = DATA_PATH.joinpath(filename_to_test)
 
-    result = process(DATA_PATH.joinpath("account_info.zip"))
-    df_result = result[0]["data_frame"]
-    assert_frame_equal(df_result, df_expected)
+    flow = process()
+
+    file_prompt = flow.send(None)
+    assert file_prompt["cmd"] == 'prompt'
+    assert file_prompt["prompt"]["type"] == 'file'
+
+    file_prompt = flow.send(str(file_to_test))
+    assert_frame_equal(file_prompt["result"][0]['data_frame'], df_expected)
